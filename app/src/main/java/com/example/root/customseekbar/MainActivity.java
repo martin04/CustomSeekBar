9c0defba2750f2713a46cac6477a27776eb508b8
mr <mr@medialab.io>
2016-08-26 10:32:25 +0200

    asda

--- app/src/main/java/com/example/root/customseekbar/MainActivity.java
+++ app/src/main/java/com/example/root/customseekbar/MainActivity.java
@@ -2,22 +2,69 @@ package com.example.root.customseekbar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et;
+    SeverityBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeverityBar bar = new SeverityBar(this);
+        et = (EditText) findViewById(R.id.editText);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!TextUtils.isEmpty(editable.toString()))
+                    bar.setProgress(Integer.parseInt(editable.toString()));
+            }
+        });
+
+        bar = new SeverityBar(this);
         bar.setDotsSize(25);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         bar.setLayoutParams(params);
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    et.setText(String.valueOf(i));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.idid);
         rl.addView(bar);
+
+
     }
 }
